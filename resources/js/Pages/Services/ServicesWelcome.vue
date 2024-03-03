2e87deb88ce4d7a67a76eff686e111fb00c1a882
Antoine <49106208+Ayolos@users.noreply.github.com>
2024-03-03 21:06:21 +0100

    modify service page mobile

--- resources/js/Pages/Services/ServicesWelcome.vue
+++ resources/js/Pages/Services/ServicesWelcome.vue
@@ -77,7 +77,7 @@ onUnmounted(() => {
             </section>
             <section class="pb-20">
                 <h4 class="text-4xl font-bold dark:text-white pb-16">Nos travaux</h4>
-                <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+                <div class="grid grid-cols-1 aspect-square md:grid-cols-3 gap-4">
                     <div v-for="i in data?.pageServicesCollection?.items[0]?.galleriesCollection?.items" class="object-cover">
                         <img class="h-auto max-w-full rounded-lg h-full aspect-square object-cover" :src="i.url" alt="">
                     </div>
